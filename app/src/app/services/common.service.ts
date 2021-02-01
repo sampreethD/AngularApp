a1ec03ee4c17f28831e88665f2091ec8e487b7a2
sampreeth <sampreeth.d@neutrinos.co>
2021-02-01 16:02:41 +0530

    try2

--- app/src/app/services/common.service.ts
+++ app/src/app/services/common.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class CommonService {
   private movieObservable = new BehaviorSubject<any>({});
@@ -13,7 +13,7 @@ export class CommonService {
 
   async callApi() {
     return new Promise((resolve, reject) => {
-      this.http.get("/* http://api.tvmaze.com/shows?page=1").subscribe(
+      this.http.get('/* http://api.tvmaze.com/shows?page=1 200').subscribe(
         (res) => {
           return resolve(res);
         },
